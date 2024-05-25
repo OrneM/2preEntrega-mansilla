1d6554b4add2f2a24e3a3ca9750b621d3a74fabc
Ornela <ornemans88@gmail.com>
2024-05-25 19:43:12 -0300

    Realice cambios en js

--- script.js
+++ script.js
@@ -1,49 +1,4 @@
-    //Creación del calendario 
 
-    const monthYearElement = document.getElementById("month-year")
-    const datesElement = document.getElementById("dates")
-    const prevBtn = document.getElementById("prev")
-    const nextBtn = document.getElementById("next")
-
-    const monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-
-    let currentDate = new Date()
-
-    function renderCalendar() {
-        const year = currentDate.getFullYear()
-        const month = currentDate.getMonth()
-        const firstDayIndex = new Date(year, month, 1).getDay()
-        const lastDay = new Date(year, month + 1, 0).getDate()
-        
-        monthYearElement.textContent = `${monthNames[month]} ${year}`
-
-        datesElement.innerHTML = ''
-
-        for (let i = 0; i < firstDayIndex; i++) {
-            datesElement.innerHTML += `<div class="date empty"></div>`
-        }
-
-        for (let i = 1; i <= lastDay; i++) {
-            let divDay = document.createElement('div')
-            divDay.className = "date"
-            divDay.textContent = i
-            datesElement.appendChild(divDay)
-            }
-    }
-
-
-    function changeMonth(direction) {
-        currentDate.setMonth(currentDate.getMonth() + direction)
-        renderCalendar()
-    }
-
-    prevBtn.addEventListener('click', () => changeMonth(-1))
-    nextBtn.addEventListener('click', () => changeMonth(1))
-
-    renderCalendar()
-   
-
-/*_______________________________________________________________________________________________*/
 // Interacción con el usuario seleccionando cantidad de  personas
 // Guardar cantidad de personas en el localStorage 
 
@@ -78,3 +33,49 @@ function agregarPersonas(e) {
     agregarInput.focus();
     agregarForm.reset();
 }
+    
+/*_______________________________________________________________________________________________*/  
+
+//Creación del calendario 
+
+const monthYearElement = document.getElementById("month-year")
+const datesElement = document.getElementById("dates")
+const prevBtn = document.getElementById("prev")
+const nextBtn = document.getElementById("next")
+
+const monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+let currentDate = new Date()
+
+function renderCalendar() {
+    const year = currentDate.getFullYear()
+    const month = currentDate.getMonth()
+    const firstDayIndex = new Date(year, month, 1).getDay()
+    const lastDay = new Date(year, month + 1, 0).getDate()
+        
+    monthYearElement.textContent = `${monthNames[month]} ${year}`
+
+    datesElement.innerHTML = ''
+
+    for (let i = 0; i < firstDayIndex; i++) {
+        datesElement.innerHTML += `<div class="date empty"></div>`
+     }
+
+    for (let i = 1; i <= lastDay; i++) {
+        let divDay = document.createElement('div')
+        divDay.className = "date"
+        divDay.textContent = i
+        datesElement.appendChild(divDay)
+        }
+    }
+
+
+    function changeMonth(direction) {
+        currentDate.setMonth(currentDate.getMonth() + direction)
+        renderCalendar()
+    }
+
+    prevBtn.addEventListener('click', () => changeMonth(-1))
+    nextBtn.addEventListener('click', () => changeMonth(1))
+
+    renderCalendar()
